b67662db8bb09625f9a2bcfcc29a3871b0728945
zohaibxJackie <zkhan52607@gmail.com>
2025-11-19 12:10:04 +0500

    dashboard changes

--- src/pages/wholesaler/Dashboard.tsx
+++ src/pages/wholesaler/Dashboard.tsx
@@ -391,7 +391,7 @@ export default function WholesalerDashboard() {
             </Select>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
             {filteredProducts.map((product) => (
               <Card key={product.id} className="flex flex-col" data-testid={`product-card-${product.id}`}>
                 <CardHeader className="pb-3">
